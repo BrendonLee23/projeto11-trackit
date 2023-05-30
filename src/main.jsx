1e42a280e4c124a85749a24661e373995cbc796e
Brendo Moreira <brendo_prime@hotmail.com>
2023-05-30 16:48:14 -0300

    organização de pastas e arquivos padrões

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import GlobalStyle from './style/GlobalStyle.js'
+import Reset from './style/ResetStyle.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
+    <Reset/>
     <App />
   </React.StrictMode>,
 )

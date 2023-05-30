83f227f531f13025fd2255c401f1ed2069597479
Brendo Moreira <brendo_prime@hotmail.com>
2023-05-30 19:15:44 -0300

    importação de fonte

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -2,7 +2,10 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
 
-	* { user-select: none; }
+	* { user-select: none;
+		box-sizing: border-box;
+		font-family: 'Recursiva', sem serifa;
+	}
 `
 
 export default GlobalStyle
